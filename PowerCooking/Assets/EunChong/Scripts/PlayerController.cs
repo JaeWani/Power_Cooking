207a28c1daeea13675491873438b60928677a6b2
김은총 <eunchonggim999@gmail.com>
2024-01-02 19:54:32 +0900

    feat : 플레이어 주방가전 바라보기 구현

--- PowerCooking/Assets/EunChong/Scripts/PlayerController.cs
+++ PowerCooking/Assets/EunChong/Scripts/PlayerController.cs
@@ -6,7 +6,8 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] InputAction mouseClickAction;
+    [SerializeField] InputAction moveInput;
+    [SerializeField] InputAction prepareCookInput;
     [SerializeField] float moveSpeed;
     [SerializeField] float rotationSpeed;
 
@@ -17,28 +18,34 @@ public class PlayerController : MonoBehaviour
     CharacterController characterController;
 
     int floorLayer;
+    int kitchenApplianceLayer;
 
     private void Awake()
     {
         mainCamera = Camera.main;
         characterController = GetComponent<CharacterController>();
         floorLayer = LayerMask.NameToLayer("Floor");
+        kitchenApplianceLayer = LayerMask.NameToLayer("KitchenAppliance");
     }
 
     private void OnEnable()
     {
         // 입력 함수 활성화
-        mouseClickAction.Enable();
+        moveInput.Enable();
+        prepareCookInput.Enable();
         // 핸들러 연결
-        mouseClickAction.performed += Move;
+        moveInput.performed += Move;
+        prepareCookInput.performed += PrepareCook;
     }
 
     private void OnDisable()
     {
         // 핸들러 해제
-        mouseClickAction.performed -= Move;
+        moveInput.performed -= Move;
+        prepareCookInput.performed -= PrepareCook;
         // 입력 함수 비활성화
-        mouseClickAction.Disable();
+        moveInput.Disable();
+        prepareCookInput.Disable();
     }
 
     /// <summary>
@@ -49,7 +56,7 @@ public class PlayerController : MonoBehaviour
         // 카메라의 마우스 포인트 위치 할당
         Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
 
-        // 
+        // 바닥을 클릭했을 때
         if(Physics.Raycast(ray:ray, hitInfo:out RaycastHit hit) && hit.collider && hit.collider.gameObject.layer == floorLayer)
         {
             if (coroutine != null) StopCoroutine(coroutine);
@@ -66,15 +73,45 @@ public class PlayerController : MonoBehaviour
 
         while (Vector3.Distance(transform.position, target) > 0.1f)
         {
-            // 충돌 무시
-            Vector3 destination = Vector3.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
-
             // 캐릭터 컨트롤
             Vector3 direction = target - transform.position;
             Vector3 movement = direction.normalized * moveSpeed * Time.deltaTime;
             characterController.Move(movement);
 
             // 캐릭터 회전
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction.normalized), rotationSpeed * Time.deltaTime);
+
+            yield return null;
+        }
+    }
+
+    /// <summary>
+    /// 플레이어의 이동 함수입니다.
+    /// </summary>
+    private void PrepareCook(InputAction.CallbackContext context)
+    {
+        // 카메라의 마우스 포인트 위치 할당
+        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        // 주방가전을 클릭했을 때
+        if (Physics.Raycast(ray: ray, hitInfo: out RaycastHit hit) && hit.collider && hit.collider.gameObject.layer == kitchenApplianceLayer)
+        {
+            if (coroutine != null) StopCoroutine(coroutine);
+            coroutine = StartCoroutine(LookKitchenAppliance(hit.point));
+            targetPosition = hit.point;
+        }
+    }
+
+    private IEnumerator LookKitchenAppliance(Vector3 target)
+    {
+        float playerDistanceToFloor = transform.position.y - target.y;
+
+        target.y += playerDistanceToFloor;
+
+        while (Vector3.Distance(transform.position, target) > 0.1f)
+        {
+            // 캐릭터 회전
+            Vector3 direction = target - transform.position;
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction.normalized), rotationSpeed * Time.deltaTime);
 
             yield return null;

012934aca65ac93014f29b3e99e8859e20c4e88e
김은총 <eunchonggim999@gmail.com>
2024-01-03 19:52:57 +0900

    fix : 프레펩 수정

--- PowerCooking/Assets/EunChong/Scripts/GuestManager.cs
+++ PowerCooking/Assets/EunChong/Scripts/GuestManager.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class GuestManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Queue<GameObject> guests = new Queue<GameObject>();
+
+    [SerializeField] int guestAppearanceTime;
+    int currentGuestAppearanceTime;
+    [SerializeField] int guestLineUpOrder;
+    int currentGuestLineUpOrder;
+
+    public void AddGuest(string command)
     {
-        
+        //guests.Enqueue(Instantiate(guest));
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SendGuest()
     {
-        
+        //string cmd = guests.Dequeue();
+        //Debug.Log(cmd);
+    }
+
+    private void Start()
+    {
+        StartCoroutine(manageRrestaurant());
+    }
+
+    IEnumerator manageRrestaurant()
+    {
+        while (true)
+        {
+            currentGuestAppearanceTime = Random.Range(1, guestAppearanceTime + 1);
+            Debug.Log("currentGuestAppearanceTime : " + currentGuestAppearanceTime);
+            yield return new WaitForSeconds(currentGuestAppearanceTime);
+            currentGuestLineUpOrder = Random.Range(1, guestLineUpOrder + 1);
+            Debug.Log("currentGuestLineUpOrder : " + currentGuestLineUpOrder);
+            
+        }
     }
 }

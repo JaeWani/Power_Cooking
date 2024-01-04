6e09db64f38dbe1cfd335dca803e7d73c847b5d9
김은총 <eunchonggim999@gmail.com>
2024-01-04 10:19:01 +0900

    feat : 큐로 줄서기 기능 추가

--- PowerCooking/Assets/EunChong/Scripts/GuestManager.cs
+++ PowerCooking/Assets/EunChong/Scripts/GuestManager.cs
@@ -1,38 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GuestManager : MonoBehaviour
 {
-    public List<Vector3> ticketQueuePos = new List<Vector3>();
-    public List<GameObject> npcInQueue = new List<GameObject>();
+    // 큐를 저장할 변수
+    private Queue<Transform> playerQueue = new Queue<Transform>();
 
-    void Start()
+    // 플레이어의 프리팹 (큐에 추가할 대상)
+    public Transform playerPrefab;
+
+    // 줄서기 포인트
+    public Transform[] points;
+
+    public static GuestManager instance = null;
+
+    void Awake()
     {
-        // yes, I should use a for cicle, but the positions in reality would be different :)
-        ticketQueuePos.Add(new Vector3(8, 0, 0));
-        ticketQueuePos.Add(new Vector3(10, 0, 0));
-        ticketQueuePos.Add(new Vector3(12, 0, 0));
-        ticketQueuePos.Add(new Vector3(14, 0, 0));
-        ticketQueuePos.Add(new Vector3(16, 0, 0));
-
-        for (int i = 0; i < 5; i++)
+        if (null == instance)
         {
-            Instantiate(npcInQueue[i], ticketQueuePos[i], transform.rotation);
+            //이 클래스 인스턴스가 탄생했을 때 전역변수 instance에 게임매니저 인스턴스가 담겨있지 않다면, 자신을 넣어준다.
+            instance = this;
         }
     }
 
-    public void TicketAcquistato()
+    void Update()
     {
-        if (npcInQueue.Count != 0)
+        // 키보드 입력 등을 통해 새로운 플레이어를 큐에 추가하는 예시
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            npcInQueue.RemoveAt(0);
+            EnqueuePlayer();
         }
 
-        for (int i = 0; i < npcInQueue.Count; i++)
+        // 큐에서 플레이어를 제거하는 예시
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
+            DequeuePlayer();
+        }
+    }
 
-            npcInQueue[i].transform.position = ticketQueuePos[i]; //not working
+    // 큐에 플레이어 추가
+    void EnqueuePlayer()
+    {
+        if (playerQueue.Count < points.Length) 
+        {
+            Transform newPlayer = Instantiate(playerPrefab, transform.position, Quaternion.identity);
+            playerQueue.Enqueue(newPlayer);
+            UpdatePlayerPositions();
+        }
+    }
+
+    // 큐에서 플레이어 제거
+    void DequeuePlayer()
+    {
+        if (playerQueue.Count > 0)
+        {
+            Transform dequeuedPlayer = playerQueue.Dequeue();
+            Destroy(dequeuedPlayer.gameObject);
+            UpdatePlayerPositions();
+        }
+    }
+
+    // 플레이어들의 위치를 업데이트
+    void UpdatePlayerPositions()
+    {
+        int positionIndex = 0;
+        foreach (Transform player in playerQueue)
+        {
+            //player.position = points[positionIndex].position;
+            player.GetComponent<GuestMovement>().currentIndex = positionIndex;
+            positionIndex++;
         }
     }
 }
--- PowerCooking/Assets/EunChong/Scripts/GuestMovement.cs
+++ PowerCooking/Assets/EunChong/Scripts/GuestMovement.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 public class GuestMovement : MonoBehaviour
 {
+    [SerializeField] float moveSpeed;
+    public int currentIndex;
 
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, GuestManager.instance.points[currentIndex].position, Time.deltaTime * moveSpeed);
+    }
 }
